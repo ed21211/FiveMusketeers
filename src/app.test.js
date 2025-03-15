4598ad2be8e0a706f16c2948497a5b70ddee15df
Hamsini <z5494868@ad.unsw.edu.au>
2025-03-15 22:26:14 +1100

    Adding the create order route and setting up the server

--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,14 @@
-// Code taken from https://blog.logrocket.com/ci-cd-node-js-github-actions/
-const app = require("./app")
-const supertest = require("supertest")
-const request = supertest(app)
+import {createOrderWrapper, clearWrapper} from './wrapper-fns.js'
 
-describe("/test endpoint", () => {
-    it("should return a response", async () => {
-        const response = await request.get("/test")
-        expect(response.status).toBe(200)
-        expect(response.text).toBe("Hello world");
-    })
-})
+beforeEach(() => {
+    clearWrapper();
+});
+
+describe('Success Cases', () => {
+    test('successful order creation', () => {
+      expect(createOrderWrapper('personA@gmail.com', [], '221B, Bakers Street, London')).toStrictEqual({
+        error: 'Invalid order list'
+      });
+    });
+});
+  
